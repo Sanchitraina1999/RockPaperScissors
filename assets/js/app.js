4213a5f8b3c12646dd5f042597c929860514ae7a
Sanchitraina1999 <sanchitraina1999@gmail.com>
2020-04-25 18:36:27 +0530

    --- Auto-Git Commit ---

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,4 +2,5 @@ const userScore = 0;
 const botScore = 0;
 const userScore_span = document.getElementById("userScore");
 const botScore_span = document.getElementById("botScore");
-const scoreBoard_div=document.getElementById("scoreBoard");
+const scoreBoard_div=document.querySelector(".scoreBoard");
+const 
