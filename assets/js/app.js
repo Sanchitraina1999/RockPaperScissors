c19517d997732170690ddbb34337707b907c2a88
Sanchitraina1999 <sanchitraina1999@gmail.com>
2020-04-25 18:49:52 +0530

    --- Auto-Git Commit ---

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,26 +3,27 @@ const userScore = 0;
 const botScore = 0;
 const userScore_span = document.getElementById("userScore");
 const botScore_span = document.getElementById("botScore");
-const scoreBoard_div=document.querySelector(".scoreBoard");
-const result_div=document.querySelector(".result");
+const scoreBoard_div = document.querySelector(".scoreBoard");
+const result_div = document.querySelector(".result");
 const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
 
-rock_div.addEventListener('click',function(){
+main();
+
+function main() {
+  rock_div.addEventListener("click", function () {
     // console.log("clicked rock");
     game("rock");
-}
-)
+  });
 
-paper_div.addEventListener('click',function(){
+  paper_div.addEventListener("click", function () {
     // console.log("clicked paper");
     game("paper");
-}
-)
+  });
 
-scissors_div.addEventListener('click',function(){
+  scissors_div.addEventListener("click", function () {
     // console.log("clicked scissors");
     game("scissors");
+  });
 }
-)
